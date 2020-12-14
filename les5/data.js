bdd999f6b9e7b93e33eede4bf274f126005d5dfd
artemklishch <artemklishch@gmail.com>
2020-12-15 00:00:49 +0200

    teyryt

---
+++ les5/data.js
@@ -0,0 +1,3 @@
+const text = "Hello Node JS from data";
+
+module.exports = text;
---
+++ les5/index.js
@@ -0,0 +1,5 @@
+const chalk = require("chalk");
+const text = require("./data");
+
+console.log(chalk.blue("Hello Node JS"));
+console.log(chalk.blue(text));
