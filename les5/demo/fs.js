8fc9d62f8eae708cb3dd142755e3a5832a00a401
artemklishch <artemklishch@gmail.com>
2020-12-17 23:13:38 +0200

    truy

---
+++ les5/demo/fs.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+// fs.mkdir(path.join(__dirname, "test"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Папка создана");
+// });
+
+const filePath = path.join(__dirname, "test", "text.txt");
+// fs.writeFile(filePath, "Hello Node JS", (err) => {
+//   if (err) throw err;
+//   console.log("Файл создан");
+//   fs.appendFile(filePath, "\nHello Again", (err) => {
+//     if (err) throw err;
+//     console.log("Файл создан");
+//   });
+// });
+fs.readFile(filePath, "utf-8", (err, content) => {
+  if (err) throw err;
+  //   console.log("Bufer content: ", content);
+  //   const data = Buffer.from(content);
+  //   console.log("Content: ", data.toString());
+  console.log(content);
+});
