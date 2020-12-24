fcf9b405da9548f903d12fcd55504553b4510bb2
artemklishch <artemklishch@gmail.com>
2020-12-24 23:10:38 +0200

    fghdfh

--- node-express/index.js
+++ node-express/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const exphbs = require("express-handlebars");
 
 const app = express();
@@ -10,14 +10,32 @@ const hbs = exphbs.create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //   res.status(200); работает по умолчанию
   //   res.sendFile(path.join(__dirname, "views", "index.html")); так делать, если не использовать handlebars
-  res.render("index");
+  res.render("index", {
+    title: "Главная страница",
+    isHome: true,
+  });
 });
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    title: "О нас",
+  });
+});
+app.get("/courses", (req, res) => {
+  res.render("courses", {
+    title: "Курсы",
+    isCourses: true,
+  });
+});
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Добавить курс",
+    isAdd: true,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
