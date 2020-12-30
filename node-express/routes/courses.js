783a441c9edd3fbe1cecc1ec3af8f898a3194124
Artem Klishch <artemklishch@gmail.com>
2020-12-30 18:22:58 +0200

    dgdsfgdg

--- node-express/routes/courses.js
+++ node-express/routes/courses.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 const Course = require("../models/course");
+const auth = require("../middleware/auth");
 // const mongooseToObj = require('../helpers/funcsForTransform')
 // const multipleMongooseToObj = require('../helpers/funcsForTransform')
 
 router.get("/", async (req, res) => {
   // const courses = await Course.find().map((c) => JSON.parse(JSON.stringify(c)));
   const courses = await Course.find()
-    .populate("userId", "email name") // вытаскивает конкретные данные из userId
+    .populate("userId", "email name") // переходит по рефу, что в модели, и указан в свойстве через userId
     .select("price title img"); // вытаскивает конкретные данные из courses
   res.render("courses", {
     title: "Курсы",
@@ -16,14 +17,14 @@ router.get("/", async (req, res) => {
   });
 });
 
-router.post("/edit", async (req, res) => {
+router.post("/edit", auth, async (req, res) => {
   const { id } = req.body;
   delete req.body.id;
   await Course.findByIdAndUpdate(id, req.body);
   res.redirect("/courses");
 });
 
-router.post("/remove", async (req, res) => {
+router.post("/remove", auth, async (req, res) => {
   try {
     await Course.deleteOne({ _id: req.body.id });
     res.redirect("/courses");
@@ -32,7 +33,7 @@ router.post("/remove", async (req, res) => {
   }
 });
 
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", auth, async (req, res) => {
   if (!req.query.allow) {
     return res.redirect("/");
   }
